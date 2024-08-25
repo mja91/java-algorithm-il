8704cb82ed01c0ce68fb6b938ec21c4416836aad
Nash <park90072001@gmail.com>
2024-08-25 16:56:42 +0900

    add: Stack / 괄호문자제거

---
+++ algorithm/괄호문자제거.java
@@ -0,0 +1,35 @@
+package algorithm;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Stack;
+
+public class 괄호문자제거 {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        char[] input = br.readLine().toCharArray();
+        Stack stack = solution(input);
+
+        String result = "";
+        for (int i=0; i<stack.size(); i++) {
+            result += stack.get(i);
+        }
+
+        System.out.println(result);
+    }
+
+    private static Stack solution(char[] input) {
+        Stack<Character> stack = new Stack<>();
+        for (char x : input) {
+            if (x == ')') {
+                while (stack.pop() != '(');
+            } else {
+                stack.push(x);
+            }
+        }
+
+        return stack;
+    }
+}
