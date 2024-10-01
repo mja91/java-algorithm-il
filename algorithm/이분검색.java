303f9b47e2f0cc58802868b30d9867f30f4133bf
Nash <park90072001@gmail.com>
2024-10-01 10:46:30 +0900

    add: 이분검색

---
+++ algorithm/이분검색.java
@@ -0,0 +1,41 @@
+package algorithm;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class 이분검색 {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int m = sc.nextInt();
+        int[] arr = new int[n];
+        for (int i=0; i<n; i++) {
+            arr[i] = sc.nextInt();
+        }
+
+        int answer = solution(n, m, arr);
+
+        System.out.println(answer);
+    }
+
+    private static int solution(int n, int m, int[] arr) {
+        int answer = 0;
+        Arrays.sort(arr);
+        int lt = 0;
+        int rt = n-1;
+        while (lt <= rt) {
+            int mid = (lt + rt) / 2;
+            if (arr[mid] == m) {
+                answer = mid+1;
+                break;
+            }
+            if (arr[mid] > m) {
+                rt = mid-1;
+            } else {
+                lt = mid+1;
+            }
+        }
+        return answer;
+    }
+}
