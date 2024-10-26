1419b54643f611c1fd3ce503c2f7bd75347990f5
Nash <park90072001@gmail.com>
2024-10-26 21:54:29 +0900

    update: 메소드명 변경

--- algorithm/dfsbfs/DFS.java
+++ algorithm/dfsbfs/DFS.java
@@ -2,15 +2,15 @@ package algorithm.dfsbfs;
 
 public class DFS {
 
-    public void execute(int n) {
+    public void dfs(int n) {
         if (n > 0) {
-            execute(n-1);
+            dfs(n-1);
             System.out.println(n);
         }
     }
 
     public static void main(String[] args) {
         DFS dfs = new DFS();
-        dfs.execute(3);
+        dfs.dfs(3);
     }
 }
--- algorithm/dfsbfs/DFS2.java
+++ algorithm/dfsbfs/DFS2.java
@@ -2,15 +2,15 @@ package algorithm.dfsbfs;
 
 public class DFS2 {
 
-    public void execute(int n) {
+    public void dfs(int n) {
         if (n > 0) {
-            execute(n / 2);
+            dfs(n / 2);
             System.out.println(n % 2 + " ");
         }
     }
 
     public static void main(String[] args) {
         DFS2 dfs2 = new DFS2();
-        dfs2.execute(11);
+        dfs2.dfs(11);
     }
 }
