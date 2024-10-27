7cc2aae8090f4da98fb47e51cdba5f2a287cf4e6
Nash <park90072001@gmail.com>
2024-10-27 18:08:13 +0900

    add: 부분집합 구하기

---
+++ algorithm/recursive_tree_graph/부분집합구하기.java
@@ -0,0 +1,31 @@
+package algorithm.recursive_tree_graph;
+
+public class 부분집합구하기 {
+    static int n;
+    static int[] check;
+
+    public static void dfs(int l) {
+        if (l == n+1) {
+            String temp = "";
+            for (int i=1; i<=n; i++) {
+                if (check[i] == 1) {
+                    temp += i + " ";
+                }
+            }
+            if (temp.length() > 0) {
+                System.out.println(temp);
+            }
+        } else {
+            check[l] = 1;
+            dfs(l+1);
+            check[l] = 0;
+            dfs(l+1);
+        }
+    }
+
+    public static void main(String[] args) {
+        n = 3;
+        check = new int[n+1];
+        dfs(1);
+    }
+}
