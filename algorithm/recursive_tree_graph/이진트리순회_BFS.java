3d655924f8e2585fe32d9e9ed356d7fff204e34c
Nash <park90072001@gmail.com>
2024-10-29 01:49:21 +0900

    update: 코드 개선

--- algorithm/recursive_tree_graph/이진트리순회_BFS.java
+++ algorithm/recursive_tree_graph/이진트리순회_BFS.java
@@ -43,13 +43,13 @@ public class 이진트리순회_BFS {
 
     public static void main(String[] args) {
         이진트리순회_BFS tree = new 이진트리순회_BFS();
-        tree.root = new 이진트리순회_BFS.Node(1);
-        tree.root.lt = new 이진트리순회_BFS.Node(2);
-        tree.root.rt = new 이진트리순회_BFS.Node(3);
-        tree.root.lt.lt = new 이진트리순회_BFS.Node(4);
-        tree.root.lt.rt = new 이진트리순회_BFS.Node(5);
-        tree.root.rt.lt = new 이진트리순회_BFS.Node(6);
-        tree.root.rt.rt = new 이진트리순회_BFS.Node(7);
+        tree.root = new Node(1);
+        tree.root.lt = new Node(2);
+        tree.root.rt = new Node(3);
+        tree.root.lt.lt = new Node(4);
+        tree.root.lt.rt = new Node(5);
+        tree.root.rt.lt = new Node(6);
+        tree.root.rt.rt = new Node(7);
         tree.bfs(tree.root);
     }
 }
