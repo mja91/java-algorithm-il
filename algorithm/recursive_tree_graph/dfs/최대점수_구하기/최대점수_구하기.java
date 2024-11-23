ad65204b7da0b9f173f666acdd902b71d78fe7ac
Nash <park90072001@gmail.com>
2024-11-23 16:28:20 +0900

    add: 최대점수_구하기

---
+++ algorithm/recursive_tree_graph/dfs/최대점수_구하기/최대점수_구하기.java
@@ -0,0 +1,36 @@
+package algorithm.recursive_tree_graph.dfs.최대점수_구하기;
+
+import java.util.Scanner;
+
+public class 최대점수_구하기 {
+
+    private static int answer = Integer.MIN_VALUE;
+    private static int n;
+    private static int m;
+
+    public static void dfs(int l, int sum, int time, int[] ps, int[] pm) {
+        if (time > m) {
+            return;
+        }
+        if (l == n) {
+            answer = Math.max(answer, sum);
+        } else {
+            dfs(l+1, sum+ps[l], time+pm[l], ps, pm);
+            dfs(l+1, sum, time, ps, pm);
+        }
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        n = sc.nextInt();
+        m = sc.nextInt();
+        int[] ps = new int[n];
+        int[] pm = new int[n];
+        for (int i=0; i<n; i++) {
+            ps[i] = sc.nextInt();
+            pm[i] = sc.nextInt();
+        }
+        dfs(0, 0, 0, ps, pm);
+        System.out.println(answer);
+    }
+}
