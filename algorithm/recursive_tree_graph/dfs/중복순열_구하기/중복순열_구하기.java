902d100885b7ecc22b4a7d886d08c5882c420b67
Nash <park90072001@gmail.com>
2024-11-23 19:22:09 +0900

    add: 중복순열_구하기

---
+++ algorithm/recursive_tree_graph/dfs/중복순열_구하기/중복순열_구하기.java
@@ -0,0 +1,32 @@
+package algorithm.recursive_tree_graph.dfs.중복순열_구하기;
+
+import java.util.Scanner;
+
+public class 중복순열_구하기 {
+
+    private static int[] pm;
+    private static int n;
+    private static int m;
+
+    public static void dfs(int l) {
+        if (l == m) {
+            for (int x : pm) {
+                System.out.print(x+" ");
+            }
+            System.out.println();
+        } else {
+            for (int i=1; i<=n; i++) {
+                pm[l] = i;
+                dfs(l+1);
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        n = sc.nextInt();
+        m = sc.nextInt();
+        pm = new int[m];
+        dfs(0);
+    }
+}
