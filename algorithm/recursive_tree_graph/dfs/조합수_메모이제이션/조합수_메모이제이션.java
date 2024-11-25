b1e5ca539c441605b94858971abcefcf870b93ee
Nash <park90072001@gmail.com>
2024-11-25 22:17:36 +0900

    add: 조합수 메모에제이션

---
+++ algorithm/recursive_tree_graph/dfs/조합수_메모이제이션/조합수_메모이제이션.java
@@ -0,0 +1,27 @@
+package algorithm.recursive_tree_graph.dfs.조합수_메모이제이션;
+
+import java.util.Scanner;
+
+public class 조합수_메모이제이션 {
+
+    private static int[][] dy = new int[35][35];
+
+    public static int dfs(int n, int r) {
+        if (dy[n][r] > 0) {
+            return dy[n][r];
+        }
+        if (n == r || r == 0) {
+            return 1;
+        } else {
+            return dy[n][r] = dfs(n-1, r-1) + dfs(n-1, r);
+        }
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int r = sc.nextInt();
+        int answer = dfs(n, r);
+        System.out.println(answer);
+    }
+}
