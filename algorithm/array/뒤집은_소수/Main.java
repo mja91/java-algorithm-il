e1a9e3c105d4d4c39b611003b4707ee67824158e
Nash <park90072001@gmail.com>
2024-12-19 00:53:52 +0900

    add: 뒤집은_소수

---
+++ algorithm/array/뒤집은_소수/Main.java
@@ -0,0 +1,48 @@
+package algorithm.array.뒤집은_소수;
+
+import java.util.*;
+
+public class Main {
+
+    static boolean isPrime(int num) {
+        if (num == 1) {
+            return false;
+        }
+        for (int i=2; i<num; i++) {
+            if (num % i == 0) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    static ArrayList<Integer> solution(int n, int[] arr) {
+        ArrayList<Integer> answer = new ArrayList<>();
+        for (int i=0; i<n; i++) {
+            int temp = arr[i];
+            int revs = 0;
+            while (temp > 0) {
+                int x = temp % 10;
+                revs = revs * 10 + x;
+                temp = temp / 10;
+            }
+            if (isPrime(revs)) {
+                answer.add(revs);
+            }
+        }
+        return answer;
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int[] arr = new int[n];
+        for (int i=0; i<n; i++) {
+            arr[i] = sc.nextInt();
+        }
+        ArrayList<Integer> answer = solution(n, arr);
+        for (int x : answer) {
+            System.out.print(x + " ");
+        }
+    }
+}
