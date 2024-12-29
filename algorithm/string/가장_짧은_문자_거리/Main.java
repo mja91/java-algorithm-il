18805137e27021e573cf6dece7bd5e5d2a2e061b
Nash <park90072001@gmail.com>
2024-12-29 23:41:15 +0900

    add: 가장_짧은_문자_거리

---
+++ algorithm/string/가장_짧은_문자_거리/Main.java
@@ -0,0 +1,39 @@
+package algorithm.string.가장_짧은_문자_거리;
+
+import java.util.*;
+
+public class Main {
+
+    static void solution(String s, String t) {
+        char[] arr = s.toCharArray();
+        char c = t.charAt(0);
+        int length = arr.length;
+        for (int i=0; i<length; i++) {
+            int lt = 0, rt = 0;
+            for (int j=i; j>=0; j--) { // <-
+                if (arr[j] == c) {
+                    break;
+                }
+                lt++;
+            }
+            for (int j=i; j<=length; j++) { // ->
+                if (arr[j] == c) {
+                    break;
+                }
+                rt++;
+            }
+            if (lt < rt) {
+                System.out.print(lt + " ");
+            } else {
+                System.out.print(rt + " ");
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String s = sc.next();
+        String t = sc.next();
+        solution(s, t);
+    }
+}
