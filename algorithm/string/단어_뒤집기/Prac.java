7cb7500b2ea50348900f04ce1d08f0b904ea36ef
Nash <park90072001@gmail.com>
2025-03-02 03:00:06 +0900

    feat: Add algorithm review

---
+++ algorithm/string/단어_뒤집기/Prac.java
@@ -0,0 +1,25 @@
+package algorithm.string.단어_뒤집기;
+
+import java.util.*;
+
+public class Prac {
+
+    static String solution(String input) {
+        StringBuilder sb = new StringBuilder();
+        int length = input.length();
+        for (int i=length-1; i>=0; i--) {
+            sb.append(input.charAt(i));
+        }
+        return sb.toString();
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        for (int i=1; i<=n; i++) {
+            String input = sc.next();
+            String answer = solution(input);
+            System.out.println(answer);
+        }
+    }
+}
